7c1f283f1d05855714e733da25432be8d7ab8625
Sungmin Kim <seongmin.kim082@gmail.com>
2020-02-02 13:40:28 -0500

    1st build and deploy

---
+++ client/build/precache-manifest.8cad918e9af6f44807465c33bca37a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0de38ca0c48d172eeb646a184b6cee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f55ff02820bfb8293171",
+    "url": "/static/css/main.8c0753e3.chunk.css"
+  },
+  {
+    "revision": "4170ae252458d92212e6",
+    "url": "/static/js/2.f21ecf9b.chunk.js"
+  },
+  {
+    "revision": "1d48674cd02fee03f10e989d5c06cba8",
+    "url": "/static/js/2.f21ecf9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f55ff02820bfb8293171",
+    "url": "/static/js/main.7132ad39.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
